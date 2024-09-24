8d2acfb7c03e675eb77f1b657df628b51dec6676
Carlin Tan <carlintan2001@gmail.com>
2024-09-24 11:38:19 +0800

    Improve UI to give a better UX

--- src/components/QuestionPageComponents/UploadJsonButton.tsx
+++ src/components/QuestionPageComponents/UploadJsonButton.tsx
@@ -8,9 +8,9 @@ import { uploadJson } from '../../controller/question-service/QuestionService';
 const codeBlockProps = {
     language: "json",
     showLineNumbers: true,
-    wrapLongLines: false,
+    wrapLongLines: true,
     theme: dracula,
-    code: '{\n  "questions": [\n        {\n            "question_title": "Add Binary",\n            "question_description": "Given two binary strings a and b, return their sum as a binary string.",\n            "question_categories": ["Bit Manipulation", "Algorithms"],\n            "question_complexity": "EASY",\n            "popularity": 10\n        },\n        {\n            "question_title": "LRU Cache Design",\n            "question_description": "Design and implement an LRU (Least Recently Used) cache.",\n            "question_categories": ["Data Structures"],\n            "question_complexity": "MEDIUM",\n            "popularity": 25\n        }\n    ]\n}',
+    code: '{\n  "questions": [\n        {\n            "question_title": "Add Binary",\n            "question_description": "Given two binary strings a and b.\\nReturn their sum as a binary string.",\n            "question_categories": ["Bit Manipulation", "Algorithms"],\n            "question_complexity": "EASY",\n            "question_popularity": 10\n        },\n        {\n            "question_title": "LRU Cache Design",\n            "question_description": "Design and implement an LRU (Least Recently Used) cache.",\n            "question_categories": ["Data Structures"],\n            "question_complexity": "MEDIUM",\n            "question_popularity": 25\n        }\n    ]\n}',
 }
 
 const UploadJsonButton = () => {
@@ -40,12 +40,13 @@ const UploadJsonButton = () => {
 
         try {
             const jsonUpload = await uploadJson(formData);
-            if (jsonUpload) { 
+            if (jsonUpload) {
                 alert('JSON file uploaded successfully.');
+                window.location.reload();
+                handleClose();
             } else {
-                alert('Error with JSON file, please try again with another JSON file.');
+                alert('Error with JSON file, please double check the format.');
             }
-            handleClose();
         } catch (error) {
             alert('Failed to upload JSON file.');
         }
@@ -53,14 +54,14 @@ const UploadJsonButton = () => {
 
     return (
         <Box>
-          <Button
+            <Button
                 variant="contained"
                 color="secondary"
                 startIcon={<UploadFileIcon />}
                 sx={{ color: 'white' }}
                 onClick={handleOpen}
             >
-            Upload JSON
+                Upload JSON
             </Button>
 
             <Modal
@@ -75,37 +76,44 @@ const UploadJsonButton = () => {
                         top: '50%',
                         left: '50%',
                         transform: 'translate(-50%, -50%)',
-                        width: 800,
+                        width: 850,
                         bgcolor: 'background.paper',
                         border: '2px solid #000',
                         boxShadow: 24,
                         p: 4,
                         borderRadius: 2,
+                        overflowY: 'auto',
+                        maxHeight: '80vh',
                     }}
                 >
                     <Typography id="modal-title" variant="h6" component="h2" sx={{ color: 'white' }}>
                         Upload JSON file
                     </Typography>
+                    <Typography variant="body2" sx={{ color: 'white' }}>
+                        Upload a JSON file following the format below. This makes it easier to add questions.
+                        Be mindful that incorrect formats or duplicate entries will result in an error.
+                    </Typography>
                     <Typography variant="subtitle2" sx={{ color: 'white', mt: 2 }}>
                         Example JSON file
                     </Typography>
-                    <Box sx={{ overflowX: 'auto', maxWidth: '100%', fontSize: "12px", display: 'flex' }}>
-                    <CopyBlock
-                        language={codeBlockProps.language}
-                        showLineNumbers={codeBlockProps.showLineNumbers}
-                        wrapLongLines={codeBlockProps.wrapLongLines}
-                        theme={codeBlockProps.theme}
-                        text={codeBlockProps.code}
-                    />
+                    <Box sx={{ maxWidth: '100%', fontSize: "12px", display: 'flex' }}>
+                        <CopyBlock
+                            language={codeBlockProps.language}
+                            showLineNumbers={codeBlockProps.showLineNumbers}
+                            wrapLongLines={codeBlockProps.wrapLongLines}
+                            theme={codeBlockProps.theme}
+                            text={codeBlockProps.code}
+                        />
                     </Box>
+
                     <Box sx={{ mt: 2, display: 'flex', justifyContent: 'center' }}>
                         <Stack>
-                            <input 
-                                id="file" 
+                            <input
+                                id="file"
                                 type="file"
                                 accept="application/json"
                                 style={{ color: 'white' }}
-                                onChange={ handleFileChange }
+                                onChange={handleFileChange}
                             />
                             {file && show && <Button
                                 component="label"
@@ -113,7 +121,7 @@ const UploadJsonButton = () => {
                                 color="secondary"
                                 sx={{ color: 'white', mt: 2 }}
                                 startIcon={<CloudUploadIcon />}
-                                onClick={ handleUpload }
+                                onClick={handleUpload}
                             >
                                 Upload File
                             </Button>}
