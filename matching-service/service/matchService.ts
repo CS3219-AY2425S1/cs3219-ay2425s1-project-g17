b3b471ccdaebe89422e28e45cb19d0cad528dd34
Shu Yang <shuyangk@gmail.com>
2024-10-09 21:39:43 +0800

    Add logic to only expand matching restrictions after certain time in queue

--- matching-service/service/matchService.ts
+++ matching-service/service/matchService.ts
@@ -10,10 +10,9 @@ export const matchUser = async (userId: string, category: string) => {
 
   if (user) {
     const difficulty: DIFFICULTY = user.difficulty as DIFFICULTY;
-
     console.log(`Attempting to match user: ${userId}, category: ${category}, difficulty: ${difficulty}`);
 
-      // Try to find an available match in the same category and difficulty (Highest Priority)
+    // Try to find an available match in the same category and difficulty (Highest Priority) for the first 10 seconds
     const potentialMatch = await Match.findOne({
       category,
       difficulty,
@@ -53,84 +52,92 @@ export const matchUser = async (userId: string, category: string) => {
       return potentialMatch;
     }
 
-    // If unable to find match with same category and difficulty, prioritise finding match with same category
-    const secondAttempt = await Match.findOne({
-      category,
-      isMatched: false,
-      userId: { $ne: userId }
-    });
+    // If unable to find match with same category and difficulty within 15 seconds, prioritise finding match with same category
+    const currentTime = Date.now();
+    if (user.createdAt < new Date(currentTime - 15000)) {
+      console.log("Expanded search... now including same category only search")
+      const secondAttempt = await Match.findOne({
+        category,
+        isMatched: false,
+        userId: { $ne: userId }
+      });
 
-    // Check if a potential match exists and if it's not the same user
-    if (secondAttempt) {
-      // Log found match
-      console.log(`Match found: User ${secondAttempt.userId} for User ${userId}`);
-      
+      // Check if a potential match exists and if it's not the same user
+      if (secondAttempt) {
+        // Log found match
+        console.log(`Match found: User ${secondAttempt.userId} for User ${userId}`);
+        
 
-      // Mark both users as matched and update partnerIds
-      user.isMatched = true;
-      user.partnerId = secondAttempt.userId;
-      secondAttempt.isMatched = true;
-      secondAttempt.partnerId = userId;
-      console.log(`User ${userId}'s match status is now: ${user.isMatched}`);
-      console.log(`User ${secondAttempt.userId}'s match status is now: ${secondAttempt.isMatched}`);
+        // Mark both users as matched and update partnerIds
+        user.isMatched = true;
+        user.partnerId = secondAttempt.userId;
+        secondAttempt.isMatched = true;
+        secondAttempt.partnerId = userId;
+        console.log(`User ${userId}'s match status is now: ${user.isMatched}`);
+        console.log(`User ${secondAttempt.userId}'s match status is now: ${secondAttempt.isMatched}`);
 
-      // Determine which category and difficulty of question both users will attempt together based on who was in queue earliest
-      if (user.createdAt <= secondAttempt.createdAt) {
-        user.difficultyAssigned = user.difficulty;
-        user.categoryAssigned = user.category;
-        secondAttempt.difficultyAssigned = user.difficulty;
-        secondAttempt.categoryAssigned = user.category;
-      } else {
-        user.difficultyAssigned = secondAttempt.difficulty;
-        user.categoryAssigned = secondAttempt.difficulty;
-        secondAttempt.difficultyAssigned = secondAttempt.difficulty;
-        secondAttempt.categoryAssigned = secondAttempt.difficulty;
+        // Determine which category and difficulty of question both users will attempt together based on who was in queue earliest
+        if (user.createdAt <= secondAttempt.createdAt) {
+          user.difficultyAssigned = user.difficulty;
+          user.categoryAssigned = user.category;
+          secondAttempt.difficultyAssigned = user.difficulty;
+          secondAttempt.categoryAssigned = user.category;
+        } else {
+          user.difficultyAssigned = secondAttempt.difficulty;
+          user.categoryAssigned = secondAttempt.difficulty;
+          secondAttempt.difficultyAssigned = secondAttempt.difficulty;
+          secondAttempt.categoryAssigned = secondAttempt.difficulty;
+        }
+
+        await secondAttempt.save();
+        await user.save();
+        console.log(`User ${secondAttempt.userId} and User ${userId} has been assigned a ${user.difficultyAssigned} question about ${user.categoryAssigned}`);
+        return secondAttempt;
       }
-
-      await secondAttempt.save();
-      await user.save();
-      console.log(`User ${secondAttempt.userId} and User ${userId} has been assigned a ${user.difficultyAssigned} question about ${user.categoryAssigned}`);
-      return secondAttempt;
     }
+    
 
-    // Lastly, find a match with same difficulty chosen
-    const thirdAttempt = await Match.findOne({
-      difficulty,
-      isMatched: false,
-      userId: { $ne: userId }
-    });
+    // Lastly, find a match with same difficulty chosen if user has been in queue for 30 seconds without a match.
+    if (user.createdAt < new Date(currentTime - 30000)) {
+      console.log("Expanded search... now including same difficulty only search")
+      const thirdAttempt = await Match.findOne({
+        difficulty,
+        isMatched: false,
+        userId: { $ne: userId }
+      });
 
-    // Check if a potential match exists and if it's not the same user
-    if (thirdAttempt) {
-      // Log found match
-      console.log(`Match found: User ${thirdAttempt.userId} for User ${userId}`);
-      
+      // Check if a potential match exists and if it's not the same user
+      if (thirdAttempt) {
+        // Log found match
+        console.log(`Match found: User ${thirdAttempt.userId} for User ${userId}`);
+        
 
-      // Mark both users as matched and update partnerIds
-      user.isMatched = true;
-      user.partnerId = thirdAttempt.userId;
-      thirdAttempt.isMatched = true;
-      thirdAttempt.partnerId = userId;
-      console.log(`User ${userId}'s match status is now: ${user.isMatched}`);
-      console.log(`User ${thirdAttempt.userId}'s match status is now: ${thirdAttempt.isMatched}`);
+        // Mark both users as matched and update partnerIds
+        user.isMatched = true;
+        user.partnerId = thirdAttempt.userId;
+        thirdAttempt.isMatched = true;
+        thirdAttempt.partnerId = userId;
+        console.log(`User ${userId}'s match status is now: ${user.isMatched}`);
+        console.log(`User ${thirdAttempt.userId}'s match status is now: ${thirdAttempt.isMatched}`);
 
-      // Determine which category and difficulty of question both users will attempt together based on who was in queue earliest
-      if (user.createdAt <= thirdAttempt.createdAt) {
-        user.difficultyAssigned = user.difficulty;
-        user.categoryAssigned = user.category;
-        thirdAttempt.difficultyAssigned = user.difficulty;
-        thirdAttempt.categoryAssigned = user.category;
-      } else {
-        user.difficultyAssigned = thirdAttempt.difficulty;
-        user.categoryAssigned = thirdAttempt.difficulty;
-        thirdAttempt.difficultyAssigned = thirdAttempt.difficulty;
-        thirdAttempt.categoryAssigned = thirdAttempt.difficulty;
+        // Determine which category and difficulty of question both users will attempt together based on who was in queue earliest
+        if (user.createdAt <= thirdAttempt.createdAt) {
+          user.difficultyAssigned = user.difficulty;
+          user.categoryAssigned = user.category;
+          thirdAttempt.difficultyAssigned = user.difficulty;
+          thirdAttempt.categoryAssigned = user.category;
+        } else {
+          user.difficultyAssigned = thirdAttempt.difficulty;
+          user.categoryAssigned = thirdAttempt.difficulty;
+          thirdAttempt.difficultyAssigned = thirdAttempt.difficulty;
+          thirdAttempt.categoryAssigned = thirdAttempt.difficulty;
+        }
+
+        await thirdAttempt.save();
+        await user.save();
+        console.log(`User ${thirdAttempt.userId} and User ${userId} has been assigned a ${user.difficultyAssigned} question about ${user.categoryAssigned}`);
+        return thirdAttempt;
       }
-
-      await thirdAttempt.save();
-      await user.save();
-      console.log(`User ${thirdAttempt.userId} and User ${userId} has been assigned a ${user.difficultyAssigned} question about ${user.categoryAssigned}`);
-      return thirdAttempt;
     }
 
     // Return null if no match found
