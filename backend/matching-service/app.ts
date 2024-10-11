d9765e1e47a7a4a235449508827bb05124868636
Shu Yang <shuyangk@gmail.com>
2024-10-11 11:38:31 +0800

    Add basic frontend, relocate matching-service to backend directory

--- backend/matching-service/app.ts
+++ backend/matching-service/app.ts
@@ -1,13 +1,14 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import mongoose from 'mongoose';
+import cors from "cors";
 import matchRoutes from './route/matchRoute';
 import './queue_worker/matchWorker';
 
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT ?? "";
 
 const connectToMongoDB = async () => {mongoose
     .connect(process.env.MONGODB_URI || '', {
@@ -22,10 +23,10 @@ const connectToMongoDB = async () => {mongoose
 // Call this function in your app.ts
 connectToMongoDB();
 
-
+app.use(cors());
 app.use(express.json());
-app.use(express.static('public')); // Serve static files from the 'public' directory
-app.use('/api/match', matchRoutes);
+//app.use(express.static('public')); // Serve static files from the 'public' directory
+app.use('/matching', matchRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
