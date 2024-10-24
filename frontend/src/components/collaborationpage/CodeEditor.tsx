de189b7eb9799b48762aa2eda82beabc05231a1e
Dominic Ng Jun Yao <e0958779@u.nus.edu>
2024-10-24 20:51:30 +0800

    Implement real-time collaborative editing

--- frontend/src/components/collaborationpage/CodeEditor.tsx
+++ frontend/src/components/collaborationpage/CodeEditor.tsx
@@ -1,33 +1,35 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Editor } from "@monaco-editor/react";
-import {
-    Box,
-    Button,
-    Snackbar,
-    Alert,
-    Dialog,
-    DialogActions,
-    DialogContent,
-    DialogContentText,
-    DialogTitle,
-    Paper,
-    Select,
-    MenuItem,
-    FormControl,
-    SelectChangeEvent,
+import { 
+    Tabs,
+    Tab, 
+    Box, 
+    Button, 
+    Snackbar, 
+    Alert, 
+    Dialog, 
+    DialogActions, 
+    DialogContent, 
+    DialogContentText, 
+    DialogTitle 
 } from '@mui/material';
-import PlayArrowIcon from '@mui/icons-material/PlayArrow';
-import CodeIcon from '@mui/icons-material/Code';
+
+import io from "socket.io-client";
+
 
 const CodeEditor = () => {
-    const [language, setLanguage] = useState<string>('javascript');
-    const [code, setCode] = useState<string>(`var message = 'Hello, World!';\nconsole.log(message);`);
-    const [isSnackbarOpen, setIsSnackbarOpen] = useState<boolean>(false);
-    const [isPopupOpen, setIsPopupOpen] = useState<boolean>(false);
+    const socket = io("http://localhost:4003");
 
-    const handleRunCode = () => {
-        console.log('Running code:', code);
-        setIsSnackbarOpen(true);
+    //TODO: Update Room ID
+    const room = "TESTROOMID123"
+    socket.emit("join_room", room);
+    const [language, setLanguage] = useState('javascript'); 
+    const [code, setCode] = useState(`var message = 'Hello, World!';\nconsole.log(message);`); 
+    const [isSnackbarOpen, setIsSnackbarOpen] = useState(false); 
+    const [isPopupOpen, setIsPopupOpen] = useState(false); 
+
+    const handleSave = () => {
+        setIsSnackbarOpen(true); 
     };
 
     const handleSubmit = () => {
@@ -39,17 +41,16 @@ const CodeEditor = () => {
     };
 
     const handleClosePopup = () => {
-        setIsPopupOpen(false);
+        setIsPopupOpen(false); 
     };
 
     const handleCloseSnackbar = () => {
-        setIsSnackbarOpen(false);
+        setIsSnackbarOpen(false); 
     };
 
-    const handleLanguageChange = (event: SelectChangeEvent<string>) => {
-        const newValue = event.target.value as string;
+    const handleLanguageChange = (event: React.SyntheticEvent, newValue: string) => {
         setLanguage(newValue);
-
+    
         switch (newValue) {
             case 'cpp':
                 setCode(`#include <iostream>\n\nint main() {\n   std::cout << "Hello, World!";\n   return 0;\n}`);
@@ -68,120 +69,131 @@ const CodeEditor = () => {
         }
     };
 
+    const sendMessage = (message: string) => {
+        socket.emit("send_message", { message, room });
+    };
+
+    const handleEditorChange = (value: string | undefined) => {
+        if (value !== undefined) {
+            setCode(value); 
+            sendMessage(value)
+        }
+    }
+
+    useEffect(() => {
+        socket.on("receive_message", (data) => {
+          console.log(data.message);
+          setCode(data.message);
+        });
+      }, [socket]);
+
     return (
-        <Paper sx={{
-            height: "90vh",
-            display: "flex",
-            flexDirection: "column"
-        }}>
-            <Box display="flex" justifyContent="flex-start" padding={1} alignItems="center">
-                <FormControl variant="standard" sx={{ minWidth: 120 }}>
-                    <Select
-                        labelId="language-select-label"
-                        value={language}
-                        sx={{ fontSize: '14px' }}
-                        onChange={handleLanguageChange}
-                    >
-                        <MenuItem value="javascript">JavaScript</MenuItem>
-                        <MenuItem value="cpp">C++</MenuItem>
-                        <MenuItem value="python">Python</MenuItem>
-                        <MenuItem value="java">Java</MenuItem>
-                    </Select>
-                </FormControl>
-            </Box>
-
-
-            <Editor
-                language={language}
-                theme="vs-dark"
-                value={code}
-                options={{
-                    fontSize: 14,
-                    formatOnType: true,
-                    autoClosingBrackets: "languageDefined",
-                    minimap: { enabled: false },
-                    padding: { top: 8 },
-                }}
-                height="80vh"
-                onChange={(newCode) => setCode(newCode || '')}
-            />
-
-            <Box display="flex" justifyContent="right" padding={1} gap={10}>
-                <Box>
-                    <Button
-                        variant="contained"
-                        color="primary"
-                        onClick={handleRunCode}
-                        sx={{ marginRight: '8px', textTransform: 'none' }}
-                        startIcon={<PlayArrowIcon />}
-                    >
-                        Run Code
-                    </Button>
-                    <Button
-                        variant="contained"
-                        color="success"
-                        onClick={handleSubmit}
-                        startIcon={<CodeIcon />}
-                        sx={{ textTransform: 'none', color: 'white',}}
-                    >
-                        Submit
-                    </Button>
-                </Box>
-            </Box>
-
-            <Snackbar
-                open={isSnackbarOpen}
-                autoHideDuration={3000}
-                onClose={handleCloseSnackbar}
+        <Box height="90%" display="flex" flexDirection="column">
+            <Tabs
+                value={language}
+                onChange={handleLanguageChange}
+                indicatorColor="primary"
+                textColor="primary"
+                centered
             >
-                <Alert onClose={handleCloseSnackbar} severity="success">
-                    Your code has been run!
-                </Alert>
-            </Snackbar>
+            <Tab label="JavaScript" value="javascript" />
+            <Tab label="C++" value="cpp" />
+            <Tab label="Python" value="python" />
+            <Tab label="Java" value="java" />
+        </Tabs>
 
-            <Dialog
-                open={isPopupOpen}
-                onClose={handleClosePopup}
-                fullWidth={false}
-                maxWidth="xs"
-                PaperProps={{
-                    sx: {
-                        padding: 2,
-                        borderRadius: '12px',
-                    },
-                }}
-            >
-                <DialogTitle sx={{ textAlign: 'center', fontWeight: 'bold' }}>
-                    Submit Code
-                </DialogTitle>
-                <DialogContent>
-                    <DialogContentText
-                        sx={{ textAlign: 'center' }}
-                    >
-                        Dom has clicked on submit. <br />
-                        Would you like to confirm the submission and end the session?
-                    </DialogContentText>
-                </DialogContent>
-                <DialogActions sx={{ justifyContent: 'center' }}>
-                    <Button
-                        onClick={handleClosePopup}
-                        color="primary"
-                        variant="outlined"
-                        sx={{ borderRadius: 5 }}
-                    >
-                        Cancel
-                    </Button>
-                    <Button
-                        onClick={handleConfirmSubmit}
-                        color="secondary"
-                        variant="contained"
-                        sx={{ borderRadius: 5, color: 'white' }}
-                    >
-                        Confirm
-                    </Button>
-                </DialogActions>
-            </Dialog>
-        </Paper>
+        <Editor
+            language={language} 
+            theme="vs-dark"
+            value={code}
+            options={{
+                fontSize: 14,
+                formatOnType: true,
+                autoClosingBrackets: "languageDefined",
+                minimap: { enabled: false },
+                padding: { top: 8 }, 
+
+            }}
+            onChange={handleEditorChange}
+        />
+
+        <Box display="flex" justifyContent="space-between" padding={2} gap={10}>
+            <Button variant="outlined" color="secondary" sx={{ borderRadius: 2 }}>
+                Show Console
+            </Button>
+            <Box>
+                <Button 
+                    variant="contained" 
+                    color="success" 
+                    onClick={handleSave} 
+                    sx={{ marginRight: '8px', borderRadius: 2 }} 
+                >
+                    Save
+                </Button>
+                <Button 
+                    variant="contained" 
+                    color="primary" 
+                    onClick={handleSubmit} 
+                    sx={{ borderRadius: 2 }}
+                >
+                    Submit
+                </Button>
+            </Box>
+        </Box>
+
+        <Snackbar
+            open={isSnackbarOpen}
+            autoHideDuration={3000}
+            onClose={handleCloseSnackbar}
+        >
+            <Alert onClose={handleCloseSnackbar} severity="success">
+                Your code has been saved!
+            </Alert>
+        </Snackbar>
+
+        <Dialog
+            open={isPopupOpen}
+            onClose={handleClosePopup}
+            fullWidth={false}  
+            maxWidth="xs" 
+            PaperProps={{
+                sx: {
+                    padding: 2,
+                    borderRadius: '12px',
+                },
+            }}
+        >
+            <DialogTitle sx={{ textAlign: 'center', fontWeight: 'bold' }}>
+                Submit Code
+            </DialogTitle>
+            <DialogContent>
+                <DialogContentText
+                    sx={{ textAlign: 'center' }}
+                >
+                    Dom has clicked on submit. <br />
+                    Would you like to confirm the submission and end the session?           
+                </DialogContentText>
+            </DialogContent>
+            <DialogActions sx={{ justifyContent: 'center' }}>
+                <Button
+                    onClick={handleClosePopup}
+                    color="secondary"
+                    variant="outlined"
+                    sx={{ borderRadius: 5 }}
+                >
+                    Cancel
+                </Button>
+                <Button
+                    onClick={handleConfirmSubmit}
+                    color="primary"
+                    variant="contained"
+                    sx={{ borderRadius: 5 }}
+                >
+                    Confirm
+                </Button>
+            </DialogActions>
+        </Dialog>
+      </Box>
     );
 };
 
