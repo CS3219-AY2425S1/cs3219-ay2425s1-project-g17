f97b4064626f0beb461d62b918c47625d6594c91
Dominic Ng Jun Yao <e0958779@u.nus.edu>
2024-10-30 19:12:40 +0800

    Store messages in Redis

--- backend/chat-service/controller/chatController.ts
+++ backend/chat-service/controller/chatController.ts
@@ -1,41 +1,23 @@
-import { Request, Response } from 'express';
-import axios from 'axios';
 import { Server, Socket } from 'socket.io';
-import { v4 as uuidv4 } from 'uuid';
+import { redisClient } from '../redisClient';
 
-let activeRooms: Record<string, { user1Id: string, user2Id: string }> = {};
-
-export const createChatRoom = async (req: Request, res: Response) => {
-    const user1Id = req.body.user1Id;
-    const user2Id = req.body.user2Id;
-
-    const sessionId = uuidv4();
-    activeRooms[sessionId] = { user1Id, user2Id };
-    res.status(200).json({ sessionId });
-};
-
-export const getChatRoom = (req: Request, res: Response) => {
-    const sessionId = req.params.sessionId;
-    const roomData = activeRooms[sessionId];
-
-    if (roomData) {
-        res.status(200).json(roomData);
-    } else {
-        res.status(404).json({ message: "Session not found" });
-    }
-};
+const saveMessage = async (message: string, sessionId: string, sender: string) => {
+    const messages = await redisClient.hgetall(sessionId);
+    const msgId = Object.keys(messages).length + 1;
+    const messageObject = { sender: sender, message: message };
+    await redisClient.hset(sessionId, msgId, JSON.stringify(messageObject));
+}
 
 export const handleSocketEvents = (io: Server) => {
     io.on("connection", (socket: Socket) => {
-        //console.log("New user connected:", socket.id);
-
         socket.on('joinSession', ({ userId, sessionId }) => {
             socket.join(sessionId);
             console.log(`User ${userId} joined room ${sessionId}`);
         });
 
-        socket.on('sendMessage', ({ sessionId, message, username }) => {
-            console.log(`Message sent to session ${sessionId} by ${username}: ${message}`);
+        socket.on('sendMessage', ({ sessionId, message, userId, username }) => {
+            console.log(`Message sent to session ${sessionId} by ${username} (${userId}): ${message}`);
+            saveMessage(message, sessionId, userId);
             socket.to(sessionId).emit('receiveMessage', { message, username });
         });
 
--- backend/chat-service/routes/chatRoutes.ts
+++ backend/chat-service/routes/chatRoutes.ts
@@ -1,9 +1,5 @@
 import express from 'express';
-import { createChatRoom, getChatRoom } from '../controller/chatController';
 
 const router = express.Router();
-
-router.post('/create', createChatRoom);
-router.get('/:sessionId', getChatRoom);
 
 export default router;
